c2e587c848a0aa17184bba3107b290fa6ee58c34
cbx <caoboxiao@gmail.com>
2016-04-22 03:37:04 -0400

    ice nuclei cluster

---
+++ src/compute_nuclei_atom.cpp
@@ -0,0 +1,325 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include "compute_nuclei_atom.h"
+#include "atom.h"
+#include "update.h"
+#include "modify.h"
+#include "neighbor.h"
+#include "neigh_list.h"
+#include "neigh_request.h"
+#include "force.h"
+#include "pair.h"
+#include "comm.h"
+#include "memory.h"
+#include "error.h"
+
+#include "group.h"
+
+using namespace LAMMPS_NS;
+
+enum{X,V,F,COMPUTE,FIX,VARIABLE};
+
+/* ---------------------------------------------------------------------- */
+
+ComputeNucleiAtom::ComputeNucleiAtom(LAMMPS *lmp, int narg, char **arg) :
+  Compute(lmp, narg, arg)
+{
+  if (narg < 4) error->all(FLERR,"Illegal compute nuclei/atom command");
+
+  if ((narg-4)%3!=0) error->all(FLERR,"Illegal compute nuclei/atom command");
+
+  nConditions=(narg-4)/3;
+  which=new int[nConditions];
+  value2index=new int[nConditions];
+  compareDirection=new bool[nConditions];
+  threshold=new double[nConditions];
+  int iCondition=0;
+  int iarg = 4;
+  while (iarg < narg) {
+    if (strncmp(arg[iarg],"c_",2) == 0 ||
+        strncmp(arg[iarg],"f_",2) == 0 ||
+        strncmp(arg[iarg],"v_",2) == 0) {
+      if (arg[iarg][0] == 'c') {
+          which[iCondition] = COMPUTE;
+          value2index[iCondition]=modify->find_compute(&arg[iarg][2]);
+          puts(&arg[iarg+1][2]);
+      //} else if (arg[iarg][0] == 'f') {
+      //    which[iCondition] = FIX;
+      //}
+      //else if (arg[iarg][0] == 'v') {
+      //    which[iCondition] = VARIABLE;
+      }
+      else error->all(FLERR,"Illegal compute diamondorder/atom command");
+
+      if (value2index[iCondition]<0) {
+          error->all(FLERR,"Illegal compute diamondorder/atom command");
+      }
+
+      if (arg[iarg+1][0] == 'g') compareDirection[iCondition] = true;
+      else if (arg[iarg+1][0] == 'l') compareDirection[iCondition] = false;
+      else error->all(FLERR,"Illegal compute diamondorder/atom command");
+
+      threshold[iCondition]=force->numeric(FLERR,arg[iarg+2]);
+
+    } else error->all(FLERR,"Illegal compute diamondorder/atom command");
+    iCondition++;
+    iarg+=3;
+  }
+
+  double cutoff = force->numeric(FLERR,arg[3]);
+  cutsq = cutoff*cutoff;
+
+  peratom_flag = 1;
+  size_peratom_cols = 0;
+  comm_forward = 1;
+
+  nmax = 0;
+  nucleiID = NULL;
+}
+
+/* ---------------------------------------------------------------------- */
+
+ComputeNucleiAtom::~ComputeNucleiAtom()
+{
+  memory->destroy(isSolid);
+  memory->destroy(nucleiID);
+}
+
+/* ---------------------------------------------------------------------- */
+
+void ComputeNucleiAtom::init()
+{
+  if (atom->tag_enable == 0)
+    error->all(FLERR,"Cannot use compute nuclei/atom unless atoms have IDs");
+  if (force->pair == NULL)
+    error->all(FLERR,"Compute nuclei/atom requires a pair style be defined");
+  if (sqrt(cutsq) > force->pair->cutforce)
+    error->all(FLERR,
+               "Compute nuclei/atom cutoff is longer than pairwise cutoff");
+
+  // need an occasional full neighbor list
+  // full required so that pair of atoms on 2 procs both set their nucleiID
+
+  int irequest = neighbor->request(this,instance_me);
+  neighbor->requests[irequest]->pair = 0;
+  neighbor->requests[irequest]->compute = 1;
+  neighbor->requests[irequest]->half = 0;
+  neighbor->requests[irequest]->full = 1;
+  neighbor->requests[irequest]->occasional = 1;
+
+  int count = 0;
+  for (int i = 0; i < modify->ncompute; i++)
+    if (strcmp(modify->compute[i]->style,"nuclei/atom") == 0) count++;
+  if (count > 1 && comm->me == 0)
+    error->warning(FLERR,"More than one compute nuclei/atom");
+}
+
+/* ---------------------------------------------------------------------- */
+
+void ComputeNucleiAtom::init_list(int id, NeighList *ptr)
+{
+  list = ptr;
+}
+
+/* ---------------------------------------------------------------------- */
+
+void ComputeNucleiAtom::compute_peratom()
+{
+  int i,j,ii,jj,inum,jnum;
+  double xtmp,ytmp,ztmp,delx,dely,delz,rsq;
+  int *ilist,*jlist,*numneigh,**firstneigh;
+
+  invoked_peratom = update->ntimestep;
+
+  // grow nucleiID array if necessary
+
+  if (atom->nlocal+atom->nghost > nmax) {
+    memory->destroy(isSolid);
+    memory->destroy(nucleiID);
+    nmax = atom->nmax;
+    memory->create(isSolid,nmax,"nuclei/atom:isSolid");
+    memory->create(nucleiID,nmax,"nuclei/atom:nucleiID");
+    vector_atom = nucleiID;
+  }
+
+  // invoke full neighbor list (will copy or build if necessary)
+
+  neighbor->build_one(list);
+
+  inum = list->inum;
+  ilist = list->ilist;
+  numneigh = list->numneigh;
+  firstneigh = list->firstneigh;
+
+  // if group is dynamic, insure ghost atom masks are current
+
+  if (group->dynamic[igroup]) {
+    commflag = 0;
+    comm->forward_comm_compute(this);
+  }
+
+  // every atom starts in its own nuclei, with nucleiID = atomID
+
+  tagint *tag = atom->tag;
+  int *mask = atom->mask;
+
+  for (ii = 0; ii < inum; ii++) {
+    i = ilist[ii];
+    if (mask[i] & groupbit) nucleiID[i] = tag[i];
+    else nucleiID[i] = 0;
+    isSolid[i]=checkSolid(i)?1:0;
+  }
+
+  // loop until no more changes on any proc:
+  // acquire nucleiIDs of ghost atoms
+  // loop over my atoms, checking distance to neighbors
+  // if both atoms are in nuclei, assign lowest nucleiID to both
+  // iterate until no changes in my atoms
+  // then check if any proc made changes
+
+  commflag = 1;
+  double **x = atom->x;
+
+  int change,done,anychange;
+
+  while (1) {
+    comm->forward_comm_compute(this);
+
+    change = 0;
+    while (1) {
+      done = 1;
+      for (ii = 0; ii < inum; ii++) {
+        i = ilist[ii];
+        if (!(mask[i] & groupbit)) continue;
+        if (!isSolid[i]) {
+            continue;
+        }
+
+        xtmp = x[i][0];
+        ytmp = x[i][1];
+        ztmp = x[i][2];
+        jlist = firstneigh[i];
+        jnum = numneigh[i];
+
+        for (jj = 0; jj < jnum; jj++) {
+          j = jlist[jj];
+          j &= NEIGHMASK;
+          if (!(mask[j] & groupbit)) continue;
+          if (nucleiID[i] == nucleiID[j]) continue;
+          if (!isSolid[j]) {
+              continue;
+          }
+
+          delx = xtmp - x[j][0];
+          dely = ytmp - x[j][1];
+          delz = ztmp - x[j][2];
+          rsq = delx*delx + dely*dely + delz*delz;
+          if (rsq < cutsq) {
+            nucleiID[i] = nucleiID[j] = MIN(nucleiID[i],nucleiID[j]);
+            done = 0;
+          }
+        }
+      }
+      if (!done) change = 1;
+      if (done) break;
+    }
+
+    // stop if all procs are done
+
+    MPI_Allreduce(&change,&anychange,1,MPI_INT,MPI_MAX,world);
+    if (!anychange) break;
+  }
+}
+
+/* ---------------------------------------------------------------------- */
+
+int ComputeNucleiAtom::pack_forward_comm(int n, int *list, double *buf,
+                                          int pbc_flag, int *pbc)
+{
+  int i,j,m;
+
+  m = 0;
+  if (commflag) {
+          for (i = 0; i < n; i++) {
+              j = list[i];
+              buf[m++] = isSolid[j];
+              buf[m++] = nucleiID[j];
+          }
+  } else {
+    int *mask = atom->mask;
+    for (i = 0; i < n; i++) {
+      j = list[i];
+      buf[m++] = ubuf(mask[j]).d;
+    }
+  }
+
+  return m;
+}
+
+/* ---------------------------------------------------------------------- */
+
+void ComputeNucleiAtom::unpack_forward_comm(int n, int first, double *buf)
+{
+  int i,m,last;
+
+  m = 0;
+  last = first + n;
+  if (commflag)
+        for (i = first; i < last; i++) {
+            isSolid[i] = buf[m++];
+            nucleiID[i] = buf[m++];
+        }
+  else {
+    int *mask = atom->mask;
+    for (i = first; i < last; i++) mask[i] = (int) ubuf(buf[m++]).i;
+  }
+}
+
+/* ----------------------------------------------------------------------
+   memory usage of local atom-based array
+------------------------------------------------------------------------- */
+
+double ComputeNucleiAtom::memory_usage()
+{
+  double bytes = nmax * sizeof(double);
+  return bytes;
+}
+
+bool ComputeNucleiAtom::checkSolid(int i) const {
+    bool ret=true;
+    int m;
+    for (m=0;m<nConditions;m++) {
+        int wh=which[m];
+        int vidx=value2index[m];
+        bool cd=compareDirection[m];
+        double td=threshold[m];
+        if (wh==COMPUTE) {
+            Compute *compute=modify->compute[vidx];
+            double *comp_vec=compute->vector_atom;
+            if (cd) {
+                ret=ret&&comp_vec[i]>td;
+            }
+            else {
+                ret=ret&&comp_vec[i]<td;
+            }
+        }
+        if (ret==false) {
+            break;
+        }
+    }
+    return ret;
+}
---
+++ src/compute_nuclei_atom.h
@@ -0,0 +1,82 @@
+/* -*- c++ -*- ----------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifdef COMPUTE_CLASS
+
+ComputeStyle(nuclei/atom,ComputeNucleiAtom)
+
+#else
+
+#ifndef LMP_COMPUTE_NUCLEI_ATOM_H
+#define LMP_COMPUTE_NUCLEI_ATOM_H
+
+#include "compute.h"
+
+namespace LAMMPS_NS {
+
+class ComputeNucleiAtom : public Compute {
+ public:
+  ComputeNucleiAtom(class LAMMPS *, int, char **);
+  ~ComputeNucleiAtom();
+  void init();
+  void init_list(int, class NeighList *);
+  void compute_peratom();
+  int pack_forward_comm(int, int *, double *, int, int *);
+  void unpack_forward_comm(int, int, double *);
+  double memory_usage();
+
+ private:
+  int nConditions;
+  int *which,*value2index;
+  bool *compareDirection;// true for greater than, false for less than
+  double *threshold;
+
+  int nmax,commflag;
+  double cutsq;
+  class NeighList *list;
+  double *nucleiID;
+  double *isSolid;
+  bool checkSolid(int m) const;
+};
+
+}
+
+#endif
+#endif
+
+/* ERROR/WARNING messages:
+
+E: Illegal ... command
+
+Self-explanatory.  Check the input script syntax and compare to the
+documentation for the command.  You can use -echo screen as a
+command-line option when running LAMMPS to see the offending line.
+
+E: Cannot use compute nuclei/atom unless atoms have IDs
+
+Atom IDs are used to identify nuclei.
+
+E: Compute nuclei/atom requires a pair style be defined
+
+This is so that the pair style defines a cutoff distance which
+is used to find nuclei.
+
+E: Compute nuclei/atom cutoff is longer than pairwise cutoff
+
+Cannot identify nuclei beyond cutoff.
+
+W: More than one compute nuclei/atom
+
+It is not efficient to use compute nuclei/atom  more than once.
+
+*/
